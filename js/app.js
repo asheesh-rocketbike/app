aaeab39c00eed74ba9bec1fd4fafd50d24bfb4cc
asheesh-rocketbike <101256072+asheesh-rocketbike@users.noreply.github.com>
2022-03-23 16:42:44 +0530

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,67 +1,67 @@
-const accordionInfo = [
+const employeeInfo = [
 	{
-      title: 'Runing Shoes', 
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-      type: 'shoes',
-      name: 'Addidas',
-      showDetail: true
+      Name: 'David', 
+      DOB: '04-10-1978',
+	  Address: 'Chandigarh',
+      Company: 'Infosis',
+      Email: 'abc@gmail.com',
+	  Phone: 19123456
     },
 	{
-      title: 'T-shirt', 
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-      type: 'cloths',
-      name: 'Roadster',
-      showDetail: false
+      Name: 'Kumar', 
+      DOB: '10-03-1988',
+	  Address: 'Delhi',
+      Company: 'HCL',
+      Email: 'xyz@gmail.com',
+	  Phone: 191236
     },
 	{
-      title: 'Shirt', 
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-      type: 'cloths',
-      name: '',
-      showDetail: false
-    },
-	{
-      title: 'Full Sleeve Shirt', 
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-      type: 'cloths',
-      name: 'HighLander',
-      showDetail: false
-    },
-	{
-      title: 'Sports Shoes', 
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-      type: 'shoes',
-      name: 'Bata',
-      showDetail: false
+      Name: 'Roof', 
+      DOB: '23-10-1998',
+	  Address: 'Paryagraj',
+      Company: 'Bebo Technology',
+      Email: 'roof@gmail.com',
+	  Phone: 19983456
     }
+	
 ]
 
-const accordion = new Vue({
-	el: "#accordion",
+const employee = new Vue({
+	el: "#employee",
 	data: {
-		title: 'Shop by Category',
-		accordionList: accordionInfo,
-		type: ''
+		title: 'Employee Detail',
+		employeeList: employeeInfo,
+		e_Name: '',
+		e_DOB: '', 
+		e_Address: '', 
+		e_Company: '', 
+		e_Email: '', 
+		e_Phone: '', 
+		error: false
 	},
 	methods: {
-		toggleDetails: function(accordionInfo){
-			accordionInfo.showDetail = !accordionInfo.showDetail
-		},
-		filterList: function(){
-			this.type = event.target.value;
+		addEmployee: function(){
+			if(!this.e_Name || !this.e_DOB || !this.e_Address || !this.e_Company || !this.e_Email || !this.e_Phone){
+				this.error = true;
+			}else{
+				this.employeeList.push({
+				  Name: this.e_Name,
+				  DOB: this.e_DOB,
+				  Address: this.e_Address,
+				  Company: this.e_Company,
+				  Email: this.e_Email,
+				  Phone: this.e_Phone
+				});
+				this.e_Name = '';
+				this.e_DOB = '';
+				this.e_Address = '';
+				this.e_Company = '';
+				this.e_Email = '';
+				this.e_Phone = '';
+				this.error = false;
+			}
 		}
-	},
-	computed: {
-      uniqueItemsList: function(){
-        const types = [];
-        this.accordionList.forEach((item)=>{
-          if(!types.includes(item.type)){
-            types.push(item.type);
-          }
-        });
-        return types;
-      }
-    }
+	}
 });
 
 
